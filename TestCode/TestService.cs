0d928f5cb97fbe84c03a2dd2ca0cf36dd537b0ff
testaccount67 <nirajg@nirajg-dev2.redmond.corp.microsoft.com>
2016-03-25 14:36:22 -0700

    A sample xUnit Project

---
+++ TestCode/TestService.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.Remoting.Messaging;
+using ProductCode;
+using Xunit;
+
+namespace MyFirstUnitTests
+{
+    public class TestService
+    {
+        public Service s;
+
+        public TestService()
+        {
+            s = new Service();
+        }
+
+        [Fact]
+        public void PassingTest1()
+        {
+            Assert.Equal(4, s.Add(2, 2));
+        }
+
+        [Fact]
+        public void PassingTest2()
+        {
+            Assert.Equal(4, s.Add(2, 2));
+        }
+
+        [Fact]
+        public void PassingTest3()
+        {
+            Assert.Equal(4, s.Add(2, 2));
+        }
+
+        [Fact]
+        public void PassingTest4()
+        {
+            Assert.Equal(4, s.Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest1()
+        {
+            Assert.Equal(4, s.Add(2, 3));
+        }
+    }
+}
